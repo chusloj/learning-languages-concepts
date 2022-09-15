815b3f0dad6afcbb870506ddecc117bd5a899831
chusloj <chusloj@gmail.com>
2022-09-14 22:35:01 -0400

    c++ switch

---
+++ c++/switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int x = ;
+    switch (x + 1)
+    {
+    case 1:
+        cout << "result is 1";
+        break;
+    case 2:
+        cout << "result is 2";
+        break;
+    }
+}
