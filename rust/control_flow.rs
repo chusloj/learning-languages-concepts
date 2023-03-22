7c8b21562ba9ab5d3c5d5227ac31a4c12662a824
chusloj <chusloj@gmail.com>
2023-03-21 20:31:48 -0600

    conditions and control flow

---
+++ rust/control_flow.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let food = "fruit";
+
+    if food == "cookie" {
+        println!("I like cookies too!");
+    } else if food == "fruit" {
+        println!("That's healthy!")
+    } else {
+        println!("Oh, that's too bad.")
+    }
+}
